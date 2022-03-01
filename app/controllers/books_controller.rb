309a86b3ec5799745bf66f27debc66a2bde49339
sakamoto-waka <wakanmhl@gmail.com>
2022-03-01 12:06:09 +0000

    [update] nameでサインアップ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 end
