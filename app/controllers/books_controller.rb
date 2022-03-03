cc95f9ce4b1167bf219486e6062454f4ad2a1b4b
sakamoto-waka <wakanmhl@gmail.com>
2022-03-03 08:46:59 +0000

    [fix] users#show @book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,14 +22,14 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path(@book)
+    redirect_to book_path(@book), notice: 'You have updated book successfully.'
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
-  end  
+  end
 
   private
   def book_params
